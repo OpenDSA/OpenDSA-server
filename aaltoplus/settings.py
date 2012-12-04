83bd7252030b5447f6eeff9faed6aa246fa65a9f
root <root@opendsa.(none)>
2012-12-04 12:43:21 -0500

    oups

--- aaltoplus/settings.py
+++ aaltoplus/settings.py
@@ -38,8 +38,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'opendsa',         #g3et_path('test.db'),            # Or path to database file if using sqlite3.
-        'USER': 'dbadmin',                  # Not used with sqlite3.
-        'PASSWORD': 'asdo!1012',                  # Not used with sqlite3.
+        'USER': '',                  # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
