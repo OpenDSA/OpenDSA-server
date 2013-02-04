eedf16edfb2a4327ae9e97fd656f922e36cc774c
breakid <breakid@vt.edu>
2013-02-04 17:18:13 -0500

    Manually added developer view files after repository conflict

--- aaltoplus/opendsa/developerview.py
+++ aaltoplus/opendsa/developerview.py
@@ -106,16 +106,20 @@ def student_list(request):
     return render_to_response("developer_view/student_list.html", {'students': students })
 
 class document_ready_activity:
-    def __init__(self, module, activity_num):
+    def __init__(self, module, activity_num, max):
         
         self.module = module
         self.activity_num = activity_num
+        self.max = max
      
     def update_activity(self):
         self.activity_num = self.activity_num + 1
         
+    def set_max(self, max):
+        self.max = max
+        
     def get_activity_num_print(self):
-        return self.activity_num*8
+        return float(self.activity_num)/float(self.max)*1000.0
     
 class proficient_exercises:
     def __init__(self, date, exercises, number):
@@ -131,27 +135,33 @@ class proficient_exercises:
         return self.number*20
 
 def student_exercise(request, student):
-    userButtons = UserButton.objects.filter(user=student)
+    userButtons = UserButton.objects.filter(user=student).filter(name='document-ready')
 
     activities = []
     max = 0
     
     for userButton in userButtons:
         flag = 0
-        if userButton.name == 'document-ready':
-            for activity in activities:
-                if activity.module == userButton.module:
-                    activity.update_activity()
-                    flag = 1
-                    break
-            if flag == 0:
-                doc_activity = document_ready_activity(userButton.module, 1)
-                activities.append(doc_activity)
+        for activity in activities:
+            if activity.module == userButton.module:
+                activity.update_activity()
+                if max < activity.activity_num:
+                    max = activity.activity_num
+                flag = 1
+                break
+        if flag == 0:
+            doc_activity = document_ready_activity(userButton.module, 1, 0)
+            if max < doc_activity.activity_num:
+                max = doc_activity.activity_num
+            activities.append(doc_activity)
         #else:
          #   if userButton.name == 'jsav-forward' or userButton.name == 'jsav-backward':
          #       doc_activity.update_activity()
     
     #number = len(activities)
+    
+    for activity in activities:
+        activity.set_max(max)
     
     userExercises = UserExercise.objects.filter(user=student).order_by('proficient_date')
     
@@ -170,7 +180,7 @@ def student_exercise(request, student):
                 temp = [userExercise.exercise]
                 exercises.append(proficient_exercises(date, temp, 1))
         
-    return render_to_response("developer_view/student_exercise.html", {'activities': activities, 'student': student, 'exercises': exercises })
+    return render_to_response("developer_view/student_exercise.html", {'activities': activities, 'student': student, 'exercises': exercises, 'max': max })
 
 def exercise_list(request, student, module):
     userButtons = UserButton.objects.filter(user=student).filter(module=module)
@@ -188,6 +198,7 @@ def exercise_list(request, student, module):
         
     return render_to_response("developer_view/exercise_list.html", {'exercises': exercises, 'student': student, 'module':module })
 
+#class of detailed exercise steps
 class exercise_step:
     def __init__(self, step_num, time, click_num, is_backward):
         
@@ -196,16 +207,24 @@ class exercise_step:
         self.time = time
         self.is_backward = is_backward
         
+    def set_max_time(self, max_time):
+        self.max_time = max_time
+        
+    def set_max_click(self, max_click):
+        self.max_click = max_click
+        
     def get_time_print(self):
-        return self.time*5
+        return float(self.time)/float(self.max_time)*500.0
     
     def get_click_num_print(self):
-        return self.click_num*10
+        return float(self.click_num)/float(self.max_click)*500.0
     
-def exercise_detail(request, student, module, exercise):
-    userButtons = UserButton.objects.filter(user=student).filter(module=module).filter(exercise=exercise)
-
-    max = 0
+def exercise_detail(request, student, exercise):
+    #The activities of a user and an exercise
+    userButtons = UserButton.objects.filter(user=student).filter(exercise=exercise)
+    #filter(module=module).
+    max_time = 0
+    max_click = 0
 
     exe_steps = []
     current_time = 0
@@ -213,6 +232,7 @@ def exercise_detail(request, student, module, exercise):
     lines = []
     line = []
 
+    #only jsav-forward and jsav-backward actions are handled
     for userButton in userButtons:
         flag = 0
 
@@ -223,7 +243,8 @@ def exercise_detail(request, student, module, exercise):
                 current_time = userButton.action_time
                 current_step = step
                 if line:
-                    lines.append(line)
+                    if not len(line) == 1:
+                        lines.append(line)
                     line = []
                 line.append(current_step)
                 continue
@@ -236,15 +257,19 @@ def exercise_detail(request, student, module, exercise):
                     current_time = userButton.action_time
                     current_step = step
                     line.append(current_step)
-                    if max < exe_step.time:
-                        max = exe_step.time
+                    if max_time < exe_step.time:
+                        max_time = exe_step.time
+                    if max_click < exe_step.click_num:
+                        max_click = exe_step.click_num
                     break
                 
             if flag == 0:
                 time_diff = int((userButton.action_time - current_time).total_seconds())
                 exe_steps.append(exercise_step(step, time_diff, 1, 'false'))
-                if max < time_diff:
-                     max = time_diff                
+                if max_time < time_diff:
+                     max_time = time_diff           
+                if max_click < 1:
+                    max_click = 1
                 current_time = userButton.action_time
                 current_step = step
                 line.append(current_step)
@@ -256,7 +281,8 @@ def exercise_detail(request, student, module, exercise):
                 current_time = userButton.action_time
                 current_step = step
                 if line:
-                    lines.append(line)
+                    if not len(line) == 1:
+                        lines.append(line)
                     line = []
                 line.append(current_step)
                 continue            
@@ -265,8 +291,10 @@ def exercise_detail(request, student, module, exercise):
                 if exe_step.step_num == step:
                     exe_step.time = exe_step.time + int((userButton.action_time - current_time).total_seconds())
                     exe_step.click_num = exe_step.click_num + 1
-                    if max < exe_step.time:
-                        max = exe_step.time                    
+                    if max_time < exe_step.time:
+                        max_time = exe_step.time          
+                    if max_click < exe_step.click_num:
+                        max_click = exe_step.click_num
                     exe_step.is_backward = 'true'
                     flag = 1
                     current_time = userButton.action_time
@@ -277,16 +305,23 @@ def exercise_detail(request, student, module, exercise):
             if flag == 0:
                 time_diff = int((userButton.action_time - current_time).total_seconds())                
                 exe_steps.append(exercise_step(step, time_diff, 1, 'true'))  
-                if max < time_diff:
-                     max = time_diff
+                if max_time < time_diff:
+                     max_time = time_diff
+                     
+                if max_click < 1:
+                    max_click = 1
+                    
                 current_time = userButton.action_time
                 current_step = step
                 line.append(current_step)
                 
-    if line:
+    if line and not len(line) == 1:
         lines.append(line)
         
-    number = len(exe_steps)
+    for exe_step in exe_steps:
+        exe_step.set_max_time(max_time)
+        exe_step.set_max_click(max_click)
+    #number = len(exe_steps)
     
     exe_steps = sorted(exe_steps, key=lambda exercise_step:exercise_step.step_num)
                   
@@ -299,6 +334,8 @@ def exercise_detail(request, student, module, exercise):
     if len(userExercises) == 1 and userExercises[0].is_proficient():
         
         proficient_date = userExercises[0].proficient_date
+        
+        exercise = userExercises[0].exercise
     
         userButtons = UserButton.objects.filter(action_time__gt = proficient_date).filter(user=student).filter(exercise=exercise)
         
@@ -313,11 +350,72 @@ def exercise_detail(request, student, module, exercise):
         
     else:
         proficient_date = 'not_proficient'
-
+        exercise = ''
     
     review_dates = list(set(review_dates))
     
     #print(proficificent_date)
     
-    return render_to_response("developer_view/exercise_detail.html", {'exe_steps': exe_steps, 'number':number, 'max': max, 'proficient_date': proficient_date, 'review_dates':review_dates, 'lines': lines })
+    return render_to_response("developer_view/exercise_detail.html", {'exe_steps': exe_steps, 'proficient_date': proficient_date, 'review_dates':review_dates, 'lines': lines, 'exercise':exercise })
 
+#The class to keep document ready event
+class docready_event():
+    def __init__(self, year, month, day, module):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.module = module
+        
+    def equals(self, year, month, day, module):
+        if self.year == year and self.month == month and self.day == day and self.module == module:
+            return 1;
+        return 0;
+
+#The first time line model
+def timeline_sum(request, student):
+    #This query fetches the document ready activities of a user
+    userButtons = UserButton.objects.filter(user=student).filter(name='document-ready')
+    
+    events = []
+
+    for userButton in userButtons:
+        flag = 0
+        for event in events:
+            if event.equals(userButton.action_time.year, userButton.action_time.month, userButton.action_time.day, userButton.module) == 1:
+                flag = 1
+                break
+        
+        if flag == 0:
+            events.append(docready_event(userButton.action_time.year, userButton.action_time.month, userButton.action_time.day, userButton.module))
+
+    #events = list(set(events))
+    for event in events:
+        print(event.month)
+
+    return render_to_response("developer_view/timeline_sum.html", {'events': events, 'student':student})
+
+#the event class to keep for the timeline detail view
+class general_event():
+    def __init__(self, year, month, day, hour, minute, second, exercise, description):
+        self.year = year
+        self.month = month
+        self.day = day
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+        self.exercise = exercise
+        self.description = description
+        
+
+#time line detail model
+def timeline_detail(request, student, module, year, month, day):
+    
+    #This query fetches the activities of a user, a model, an action date except the document ready actions
+    userButtons = UserButton.objects.filter(user=student).filter(module__name=module).filter(action_time__year=year).filter(action_time__month=month).filter(action_time__day=day).exclude(name='document-ready')
+
+    events = []
+
+    for userButton in userButtons:
+        events.append(general_event(userButton.action_time.year, userButton.action_time.month, userButton.action_time.day, userButton.action_time.hour, userButton.action_time.minute, userButton.action_time.second, userButton.exercise, userButton.description))
+
+    return render_to_response("developer_view/timeline_detail.html", {'events':events})
