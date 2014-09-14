f30a4da25b95b2748bfe452725f4a3c16fbc57b2
efouh <ericfouh@gmail.com>
2014-09-13 22:06:29 -0400

    Fixed usability issues in edit module and set exercises forms

--- ODSA-django/course/forms.py
+++ ODSA-django/course/forms.py
@@ -4,3 +4,4 @@ from exercise.exercise_models import CourseModule
 class CourseModuleForm(forms.ModelForm):
     class Meta:
         model = CourseModule
+        exclude = ['course_instance','points_to_pass','opening_time','late_submissions_allowed', 'late_submission_deadline']
