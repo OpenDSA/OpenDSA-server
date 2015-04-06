bf3b89202483fa38a2974fd6141993f5c7097198
Sally Hamouda <sallyh84@vt.edu>
2015-04-06 11:48:53 -0400

    ASCCI problem fix

--- ODSA-django/openpop/build/rectest/recw24PROG/recw24PROG.java
+++ ODSA-django/openpop/build/rectest/recw24PROG/recw24PROG.java
@@ -1,7 +1,5 @@
 /** Test program for recursion programming exercise.
 Author: Sally Hamouda */
-//Exercise 24: outputs the minimal number of invocations of the operations +1 and ∗2 that are required to obtain Y from X. For Example: If the inputs 10 17, the output will be 7 (due to 7 invocations of +1). For the input 10 21, the output will be 2 (due to ∗2 and then +1).
-
 
 import java.io.*;
 import java.util.Random;
@@ -35,7 +33,7 @@ else
     int x= randNumGenerator.nextInt(100);
     int y= randNumGenerator.nextInt(100);
     int temp= x;
-    // x has to be greater than y by problem definition 
+  
     if (x>y){
      x=y;
      y=temp;
