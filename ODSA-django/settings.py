61cf854c58e0ef85061a91557eaa6cdde2a66f68
hosamshahin <hosamlshahin@gmail.com>
2015-10-27 12:42:36 -0400

    Add https://lti.cs.vt.edu for allowed origins

--- ODSA-django/settings.py
+++ ODSA-django/settings.py
@@ -16,7 +16,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 # gist middleware variables
 XS_SHARING_ALLOWED_ORIGINS = ['http://algoviz-beta6.cc.vt.edu',
-                              'http://algoviz-beta.cs.vt.edu',			      
+                              'http://algoviz-beta.cs.vt.edu',
                               'http://algoviz.org',
                               'http://algoviz-beta7.cc.vt.edu',
                               'http://opendsa.local',
@@ -26,6 +26,7 @@ XS_SHARING_ALLOWED_ORIGINS = ['http://algoviz-beta6.cc.vt.edu',
                               'http://localhost:8003',
                               'https://ltitest.cs.vt.edu:9292',
                               'https://lti.cs.vt.edu:9292',
+                              'https://lti.cs.vt.edu',
                               'https://ltitest.herokuapp.com']
 XS_SHARING_ALLOWED_METHODS = ['POST', 'GET', 'OPTIONS', 'PUT', 'DELETE']
 XS_SHARING_ALLOWED_HEADERS = ["Content-Type"]
