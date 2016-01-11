bc9d31567412b121d184d208bec1529b79c16d8a
Hosam Shahin <hosamlshahin@gmail.com>
2016-01-11 14:07:16 -0500

    internal changes

--- ODSA-django/conf.py
+++ ODSA-django/conf.py
@@ -1,5 +1,5 @@
-secret_key = 'opendsa_secret_key'
-base_path = '/vagrant'
-name = 'opendsa'
-user = 'opendsa'
-password = 'opendsa'
+name = 'odsa_S14'  #odsa_SP13
+user = 'dbadmin'
+password = 'vis4_dsa'
+secret_key = 'hf7nbgeuz8(y+&9&(7fy62aiw-5djxhfm+n4#f38*)^_-*#6e%'
+base_path = '/home'
